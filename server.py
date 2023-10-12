f968b3e98380ff237ee937bb6076e20682b08b30
Roman Pavelka <roman.pavelka.asi@gmail.com>
2023-10-13 01:03:54 +0200

    Make connection persistent on server side

--- server.py
+++ server.py
@@ -4,31 +4,46 @@ import json
 
 clients = {}
 quiz = None
-results = {}
+
+def results():
+    return str(clients)
 
 async def handler(websocket, path):
     global quiz, results
 
-    try:
-        msg = await websocket.recv()
-        data = json.loads(msg)
-        print(data)
-        if data["type"] == "register":
-            clients[data["name"]] = websocket
-            await websocket.send(json.dumps({"type": "wait"}))
-        elif data["type"] == "submit_quiz":
-            quiz = data["quiz"]
-        elif data["type"] == "start_game":
-            for ws in clients.values():
-                await ws.send(json.dumps({"type": "start", "quiz": quiz}))
-        elif data["type"] == "submit_answer":
-            results[data["name"]] = data["answer"]
-            clients[data["name"]] = None
-        elif data["type"] == "get_results":
-            await websocket.send(json.dumps({"type": "results", "results": results}))
+    while True:
+        try:
+            msg = await websocket.recv()
+            data = json.loads(msg)
+            print("Got", data)
 
-    except Exception as e:
-        print(f"Error: {e}")
+            response = None
+            if data["type"] == "register":
+                clients[data["name"]] = websocket
+                await websocket.send(json.dumps({"type": "wait"}))
+            elif data["type"] == "submit_quiz":
+                quiz = data["quiz"]
+            elif data["type"] == "start_game":
+                for ws in clients.values():
+                    if ws:  # Check if the websocket is still active
+                        response = {"type": "start", "quiz": quiz}
+            elif data["type"] == "submit_answer":
+                results[data["name"]] = data["answer"]
+                clients[data["name"]] = None
+            elif data["type"] == "get_results":
+                response = {"type": "results", "results": results()}
+
+            if response is not None:
+                response = json.dumps(response)
+                print("Sending", response)
+                await websocket.send(response)
+
+        except websockets.ConnectionClosed:
+            print(f"Connection with {websocket.remote_address} closed")
+            break
+        except Exception as e:
+            print(f"Error: {e}")
+            break
 
 start_server = websockets.serve(handler, "localhost", 8765)
 
